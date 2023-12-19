514821928680f2865995e0bfb14635e4ba5aa00e
Kurtm36 <kurtm36@gmail.com>
2023-12-19 18:52:08 +0000

    Create Comment Model for models.py

--- veggapp/models.py
+++ veggapp/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 from django.utils.text import slugify
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
 # Post Model
+
 
 class Post(models.Model):
     # Basic Post information
@@ -34,6 +36,23 @@ class Post(models.Model):
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title, allow_unicode=True)
         super().save(*args, **kwargs)
-            
+
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+        
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
+
+  
